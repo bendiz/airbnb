589635f67f16fc33aedcc0318bb23f00e1c12303
Bendiz <benerok@icloud.com>
2023-02-27 20:54:32 +0100

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import './App.css';
-
+import './App.css'
+import Navigation from './components/Navigation'
 function App() {
-  return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Navigation />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+import './Navigation.css'
+
+export default function Navigation() {
+    return (
+        <nav className="Navigation">
+            <img src="/src/img/airbnb-logo.png" className="logo"></img>
+        </nav>
+    )
+}
