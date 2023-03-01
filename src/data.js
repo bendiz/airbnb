ecfd84fe99aa2c3ddb808c93a89e6bf452015815
Bendiz <benerok@icloud.com>
2023-03-01 10:03:18 +0100

    Improve layout

--- src/data.js
+++ src/data.js
@@ -83,18 +83,4 @@ export default [
         location: 'Online',
         openSpots: 27,
     },
-    {
-        id: 7,
-        title: 'Group Mountain Biking',
-        description:
-            'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
-        price: 50,
-        coverImg: 'mountain-bike.png',
-        stats: {
-            rating: 4.8,
-            reviewCount: 2,
-        },
-        location: 'Norway',
-        openSpots: 3,
-    },
 ]
